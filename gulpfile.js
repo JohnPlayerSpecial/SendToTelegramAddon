314e7c3ff1b2878024c4d4e4a7453129e7cc7a66
CS-PRIO30 <cs.prio30@gmail.com>
2017-03-25 19:05:25 +0100

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var gulp = require('gulp');
+var zip = require('gulp-zip');
+
+var files = ['manifest.json','sendtotelegram.js','telegram32.png','telegram48.png','telegram.png'];
+var xpiName = 'Sendtotelegram.xpi';
+
+gulp.task('default', function () {
+  gulp.src(files)
+    .pipe(zip(xpiName))
+    .pipe(gulp.dest('.'));
+});
