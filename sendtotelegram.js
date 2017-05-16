50c144ef08d6ab2ee683fe9ae7c1cf6db061c1ef
CS-PRIO30 <cs.prio30@gmail.com>
2017-05-16 20:56:51 +0200

    Update sendtotelegram.js

--- sendtotelegram.js
+++ sendtotelegram.js
@@ -1,19 +1,16 @@
 function getURL(){
 	browser.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
 	var url = tabs[0].url;
-	console.log(url);
 	sendtotelegram(url);
 	});
 }
 
 browser.browserAction.onClicked.addListener( getURL );
 
-
 function sendtotelegram(url){
 	var xhr = new XMLHttpRequest();
 	TELEGRAM_TOKEN = "330039403:AAHX6l_PBmiXzVYuw-dXG3IQPdbMFNmPymk";
 	url_tg = "https://api.telegram.org/bot" + TELEGRAM_TOKEN + "/SendMessage?chat_id=31923577&text=" + encodeURIComponent(url) ;
-	console.log(url_tg);
 	xhr.open('GET', url_tg, true);
 	xhr.send();
 }
